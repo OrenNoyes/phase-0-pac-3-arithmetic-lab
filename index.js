0c2531a553e4f2bbff61004d22aac2920b10ccbf
Oren <orennoyes@Administrators-MacBook-Pro.local>
2022-06-16 14:25:11 -0600

    commit tree aaf7d44f1cc632c887748f373f8215139e496015

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+    return a + b;
+}
+
+function divide(a, b) {
+    return a / b;
+
+}
+
+function subtract(a, b) {
+    return a - b;
+
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function increment(n){
+    n +=1
+    return n 
+}
+
+function decrement(n){
+    n -= 1
+    return n 
+}
+
+function makeInt(string) {
+    string = parseInt(string, 10);
+    return string
+}
+
+function preserveDecimal(string) {
+    string = parseFloat(string)
+    return string
+}
